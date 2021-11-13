6d6296a62cd3ba7cc48cccce83e31432cd90abc9
emilkawoyciechowska <emillkawojciechowska@gmail.com>
2021-11-13 13:40:35 +0100

    calendar

--- Calories.py
+++ Calories.py
@@ -1,10 +1,13 @@
 import requests
 
 api_url = 'https://api.calorieninjas.com/v1/nutrition?query='
-query = 'pizza'
+query = 'pdksjnk'
 response = requests.get(api_url + query, headers={'X-Api-Key': '+K83QUc3yx5viW1/jBEWqw==YJ6E1gKifoWb4kZn'})
 if response.status_code == requests.codes.ok:
-    print(response.text)
+    try:
+        print(response.json()["items"][0]["calories"])
+    except:
+        print ("wrond ")
 else:
     print("Error:", response.status_code, response.text)
 
--- MENU.py
+++ MENU.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 from PIL import Image, ImageTk
 from Profil import male_cpm_small, male_cpm_medium, male_cpm_big, female_cpm_medium,female_cpm_small,female_cpm_big
 from Profil import BMI
+import requests
 
 LARGEFONT = ("Verdana", 35)
 
@@ -210,12 +211,12 @@ class Page1(tk.Frame):
 class Page2(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text="Page 2", font=LARGEFONT)
-        label.grid(row=0, column=4, padx=10, pady=10)
+        label = ttk.Label(self, text="TODAY", font=LARGEFONT)
+        label.grid(row=0, column=3, padx=10, pady=10)
 
         # button to show frame 2 with text
         # layout2
-        button1 = ttk.Button(self, text="Page 1",
+        button1 = ttk.Button(self, text="CALCULATOR",
                              command=lambda: controller.show_frame(Page1))
 
         # putting the button in its place by
@@ -224,12 +225,114 @@ class Page2(tk.Frame):
 
         # button to show frame 3 with text
         # layout3
-        button2 = ttk.Button(self, text="Startpage",
+        button2 = ttk.Button(self, text="MENU",
                              command=lambda: controller.show_frame(StartPage))
 
         # putting the button in its place by
         # using grid
         button2.grid(row=2, column=1, padx=10, pady=10)
+
+
+        sniadL = tk.Label(self,text="BRAKFAST")
+        sniadL.grid (row=1,column=2)
+
+        sniad = tk.Entry(self)
+        sniad.grid(row=1, column=3)
+        sniadGL= tk.Label(self,text="gram")
+        sniadGL.grid(row=1,column=5)
+        sniadG = tk.Entry(self)
+        sniadG.grid(row=1, column=4)
+
+        obiadL = tk.Label(self,text="LUNCH")
+        obiadL.grid(row=2,column=2)
+        obiad = tk.Entry(self)
+        obiad.grid(row=2, column=3)
+        obiadG = tk.Entry(self)
+        obiadG.grid(row=2,column=4)
+        obiadGL = tk.Label(self, text="gram")
+        obiadGL.grid(row=2, column=5)
+
+        kolacjaL = tk.Label(self,text="DINNER")
+        kolacjaL.grid(row=3, column=2)
+
+        kolacja = tk.Entry(self)
+        kolacja.grid(row=3, column=3)
+        kolacjaG=tk.Entry(self)
+        kolacjaG.grid(row=3,column=4)
+
+        kolGL = tk.Label(self, text="gram")
+        kolGL.grid(row=3, column=5)
+
+
+        wynikL = tk.Label(self)
+        wynikL.grid(row=5,column=3)
+
+        kcal = tk.Label(self,text="kcal")
+        kcal.grid(row=5,column=4)
+        wynikL2 = tk.Label(self)
+        wynikL2.grid(row=6, column=3)
+        kcal2 = tk.Label(self,text="kcal")
+        kcal2.grid(row=6, column=4)
+        wynikL3 = tk.Label(self)
+        wynikL3.grid(row=7, column=3)
+        kcal3 = tk.Label(self,text="kcal")
+        kcal3.grid(row=7, column=4)
+
+        wronglabel = tk.Label(self)
+        wronglabel.grid(row=9,column=3)
+        def APIB():
+            api_url = 'https://api.calorieninjas.com/v1/nutrition?query='
+            query = sniad.get()
+            response = requests.get(api_url + query, headers={'X-Api-Key': '+K83QUc3yx5viW1/jBEWqw==YJ6E1gKifoWb4kZn'})
+            if response.status_code == requests.codes.ok:
+                try:
+                    wynikL.config(text=int(response.json()["items"][0]["calories"])/100*int(sniadG.get()))
+                except:
+                    wronglabel.config(text="This food is not in food base")
+
+            else:
+                wynikL.config(text="This food is not in food base")
+
+
+        def APIL():
+            api_url = 'https://api.calorieninjas.com/v1/nutrition?query='
+            query = obiad.get()
+            response = requests.get(api_url + query, headers={'X-Api-Key': '+K83QUc3yx5viW1/jBEWqw==YJ6E1gKifoWb4kZn'})
+            if response.status_code == requests.codes.ok:
+                 try:
+
+                     display_tekst = int(response.json()["items"][0]["calories"])/100*int(obiadG.get())
+                     wynikL2.config(text=display_tekst)
+                 except:
+                    wronglabel.config(text="This food is not in food base")
+
+            else:
+                wynikL2.config(text="This food is not in food base")
+
+        def APID():
+            api_url = 'https://api.calorieninjas.com/v1/nutrition?query='
+            query = kolacja.get()
+            response = requests.get(api_url + query, headers={'X-Api-Key': '+K83QUc3yx5viW1/jBEWqw==YJ6E1gKifoWb4kZn'})
+            if response.status_code == requests.codes.ok:
+                try:
+                    wynikL3.config(text=int(response.json()["items"][0]["calories"]) / 100 * int(kolacjaG.get()))
+                except:
+                    wronglabel.config(text="This food is not in food base")
+            else:
+                wynikL3.config(text="This food is not in food base")
+
+
+
+        button3 = tk.Button(self,text="ADD",command=APIB)
+        button3.grid(row=1,column=6)
+
+        button4 = tk.Button(self, text="ADD", command=APIL)
+        button4.grid(row=2, column=6)
+
+        button5 = tk.Button(self, text="ADD", command=APID)
+        button5.grid(row=3, column=6)
+
+
 
 class Page3(tk.Frame):
     def __init__(self, parent, controller):
